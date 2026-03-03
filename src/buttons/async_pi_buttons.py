bdea40a03e8d1d4bb22dc229b60a8b7b63a2ff4b
tisauro <tisauro@gmail.com>
2026-03-03 14:58:02 +0000

    * reorganized code

--- src/buttons/async_pi_buttons.py
+++ src/buttons/async_pi_buttons.py
@@ -11,11 +11,13 @@ logger = logging.getLogger(__name__)
 
 
 class AsyncPiButtons:
-    def __init__(self,
-                 button_01_pin: int = 23,
-                 button_02_pin: int = 22,
-                 hold_time: float = 0.5,
-                 bounce_time: float = 0.05):
+    def __init__(
+        self,
+        button_01_pin: int = 23,
+        button_02_pin: int = 22,
+        hold_time: float = 0.5,
+        bounce_time: float = 0.05,
+    ):
         self._button_states = {}
         self._double_button_triggered = False  # Add this flag
         self.hold_time = hold_time
@@ -32,24 +34,28 @@ class AsyncPiButtons:
         self._loop = asyncio.get_running_loop()
         self._queue = asyncio.Queue()
         try:
-            self._button_01 = Button(self._button_01_pin,
-                                     pull_up=True,
-                                     hold_time=self.hold_time,
-                                     bounce_time=self.bounce_time)
+            self._button_01 = Button(
+                self._button_01_pin,
+                pull_up=True,
+                hold_time=self.hold_time,
+                bounce_time=self.bounce_time,
+            )
             self._button_01.when_held = self._button_01_held
             self._button_01.when_released = self._button_01_released
-        except Exception as e:
-            logger.exception(f'Failed to attach button_01 interrupt')
+        except Exception:
+            logger.exception("Failed to attach button_01 interrupt")
             raise
         try:
-            self._button_02 = Button(self._button_02_pin,
-                                     pull_up=True,
-                                     hold_time=self.hold_time,
-                                     bounce_time=self.bounce_time)
+            self._button_02 = Button(
+                self._button_02_pin,
+                pull_up=True,
+                hold_time=self.hold_time,
+                bounce_time=self.bounce_time,
+            )
             self._button_02.when_held = self._button_02_held
             self._button_02.when_released = self._button_02_released
-        except Exception as e:
-            logger.exception(f'Failed to attach button_02 interrupt')
+        except Exception:
+            logger.exception("Failed to attach button_02 interrupt")
             if self._button_01:
                 self._button_01.close()
             raise
@@ -64,7 +70,7 @@ class AsyncPiButtons:
     def _button_01_released(self, btn):
         if not self._button_states.get(btn, False):
             logger.debug("Button 01 pressed")
-            self._loop.call_soon_threadsafe(self._queue.put_nowait, 'button_01')
+            self._loop.call_soon_threadsafe(self._queue.put_nowait, "button_01")
         self._button_states[btn] = False
         if not self._button_02.is_pressed:
             self._double_button_triggered = False
@@ -72,7 +78,7 @@ class AsyncPiButtons:
     def _button_02_released(self, btn):
         if not self._button_states.get(btn, False):
             logger.debug("Button 02 pressed")
-            self._loop.call_soon_threadsafe(self._queue.put_nowait, 'button_02')
+            self._loop.call_soon_threadsafe(self._queue.put_nowait, "button_02")
         self._button_states[btn] = False
         if not self._button_01.is_pressed:
             self._double_button_triggered = False
@@ -85,9 +91,11 @@ class AsyncPiButtons:
                 if not self._double_button_triggered:
                     self._double_button_triggered = True
                     self._button_states[self._button_02] = True
-                    self._loop.call_soon_threadsafe(self._queue.put_nowait, 'double_button')
+                    self._loop.call_soon_threadsafe(
+                        self._queue.put_nowait, "double_button"
+                    )
         else:
-            self._loop.call_soon_threadsafe(self._queue.put_nowait, 'button_01_held')
+            self._loop.call_soon_threadsafe(self._queue.put_nowait, "button_01_held")
 
     def _button_02_held(self, btn):
         self._button_states[btn] = True
@@ -97,9 +105,11 @@ class AsyncPiButtons:
                 if not self._double_button_triggered:
                     self._double_button_triggered = True
                     self._button_states[self._button_01] = True
-                    self._loop.call_soon_threadsafe(self._queue.put_nowait, 'double_button')
+                    self._loop.call_soon_threadsafe(
+                        self._queue.put_nowait, "double_button"
+                    )
         else:
-            self._loop.call_soon_threadsafe(self._queue.put_nowait, 'button_02_held')
+            self._loop.call_soon_threadsafe(self._queue.put_nowait, "button_02_held")
 
     def __aiter__(self):
         return self
@@ -110,14 +120,12 @@ class AsyncPiButtons:
         return direction
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import time
-
 
     async def main():
         async with AsyncPiButtons() as buttons:
             async for direction in buttons:
                 print(f"{time.time():0.2f} - Direction: {direction}")
-
 
     asyncio.run(main())
